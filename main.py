8f94c47cb71ede2b6c3df82d9562a8bfd8d45bb9
pedrocivita <pedro.civita@gmail.com>
2024-09-04 08:25:27 -0300

    Roteiro 04 v0

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
 import sys
+from abc import ABC, abstractmethod
 
+# Classe PrePro para filtrar comentários
+class PrePro:
+    @staticmethod
+    def filter(code: str) -> str:
+        return '\n'.join([line.split('--')[0] for line in code.splitlines()])
+
+# Classe Node (abstrata) e suas subclasses
+class Node(ABC):
+    def __init__(self, value=None):
+        self.value = value
+        self.children = []
+
+    @abstractmethod
+    def Evaluate(self):
+        pass
+
+class BinOp(Node):
+    def __init__(self, value, left, right):
+        super().__init__(value)
+        self.children = [left, right]
+
+    def Evaluate(self):
+        if self.value == '+':
+            return self.children[0].Evaluate() + self.children[1].Evaluate()
+        elif self.value == '-':
+            return self.children[0].Evaluate() - self.children[1].Evaluate()
+        elif self.value == '*':
+            return self.children[0].Evaluate() * self.children[1].Evaluate()
+        elif self.value == '/':
+            return self.children[0].Evaluate() // self.children[1].Evaluate()
+
+class UnOp(Node):
+    def __init__(self, value, child):
+        super().__init__(value)
+        self.children = [child]
+
+    def Evaluate(self):
+        if self.value == '+':
+            return +self.children[0].Evaluate()
+        elif self.value == '-':
+            return -self.children[0].Evaluate()
+
+class IntVal(Node):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def Evaluate(self):
+        return self.value
+
+class NoOp(Node):
+    def Evaluate(self):
+        return 0
+
+# Classe Token
 class Token:
     def __init__(self, type: str, value: int):
         self.type = type
         self.value = value
 
-
+# Classe Tokenizer
 class Tokenizer:
     def __init__(self, source: str):
         self.source = source
@@ -49,88 +104,74 @@ class Tokenizer:
         else:
             raise ValueError(f"Unexpected character: {current_char}")
 
-
+# Classe Parser
 class Parser:
     @staticmethod
     def parseExpression():
-        result = Parser.parseTerm()
+        left = Parser.parseTerm()
 
         while Parser.tokenizer.next.type in ['PLUS', 'MINUS']:
-            op_type = Parser.tokenizer.next.type
+            op = Parser.tokenizer.next.type
             Parser.tokenizer.selectNext()
-            result2 = Parser.parseTerm()
+            right = Parser.parseTerm()
+            left = BinOp(op, left, right)
 
-            if op_type == 'PLUS':
-                result += result2
-            elif op_type == 'MINUS':
-                result -= result2
-
-        return result
+        return left
 
     @staticmethod
     def parseTerm():
-        result = Parser.parseFactor()
+        left = Parser.parseFactor()
 
         while Parser.tokenizer.next.type in ['MULT', 'DIV']:
-            op_type = Parser.tokenizer.next.type
+            op = Parser.tokenizer.next.type
             Parser.tokenizer.selectNext()
-            result2 = Parser.parseFactor()
+            right = Parser.parseFactor()
+            left = BinOp(op, left, right)
 
-            if op_type == 'MULT':
-                result *= result2
-            elif op_type == 'DIV':
-                if result2 == 0:
-                    raise ValueError("Division by zero")
-                result //= result2
-
-        return result
+        return left
 
     @staticmethod
     def parseFactor():
         if Parser.tokenizer.next.type == 'PLUS':
             Parser.tokenizer.selectNext()
-            return Parser.parseFactor()
+            return UnOp('PLUS', Parser.parseFactor())
         elif Parser.tokenizer.next.type == 'MINUS':
             Parser.tokenizer.selectNext()
-            return -Parser.parseFactor()
+            return UnOp('MINUS', Parser.parseFactor())
         elif Parser.tokenizer.next.type == 'LPAREN':
             Parser.tokenizer.selectNext()
-            result = Parser.parseExpression()
+            node = Parser.parseExpression()
             if Parser.tokenizer.next.type != 'RPAREN':
                 raise ValueError("Syntax Error: Expected ')'")
             Parser.tokenizer.selectNext()
-            return result
+            return node
         elif Parser.tokenizer.next.type == 'INT':
-            result = Parser.tokenizer.next.value
+            node = IntVal(Parser.tokenizer.next.value)
             Parser.tokenizer.selectNext()
-            return result
+            return node
         else:
-            raise ValueError("Syntax Error: Expected INT or '('")
+            return NoOp()
 
     @staticmethod
     def run(code: str):
-        try:
-            Parser.tokenizer = Tokenizer(code)
-            Parser.tokenizer.selectNext()
-            result = Parser.parseExpression()
+        Parser.tokenizer = Tokenizer(code)
+        Parser.tokenizer.selectNext()
+        return Parser.parseExpression()
 
-            if Parser.tokenizer.next.type != 'EOF':
-                raise ValueError("Syntax Error: Expected EOF at the end of expression")
-
-            print(result)
-        except Exception as e:
-            print(f"Error: {e}", file=sys.stderr)
-            sys.exit(1)
-
-
+# Programa principal
 def main():
     if len(sys.argv) > 1:
-        code = sys.argv[1]
+        file_name = sys.argv[1]
+        with open(file_name, 'r') as file:
+            code = file.read()
     else:
-        code = input()
+        print("Please provide a .lua file.")
+        return
 
-    Parser.run(code)
-
+    filtered_code = PrePro.filter(code)
+    tree = Parser.run(filtered_code)
+    result = tree.Evaluate()
+    print(result)
 
 if __name__ == "__main__":
     main()
